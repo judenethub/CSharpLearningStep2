3720598a48bc0040dc1c4febd6017fd19139bbae
judenethub <60697235+judenethub@users.noreply.github.com>
2021-01-21 15:53:29 +0300

    Add files via upload

---
+++ readonly.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Learning
+{
+    class Program
+    {
+        class User
+        {
+            public string login = "judenet", name = "vasya", surname = "Smirnov", age ="40"; 
+            public readonly string date;
+
+            public User()
+            {
+                date = "19.08.1979";
+            }
+
+           
+
+            
+        }
+        static void Main()
+        {
+            User abc = new User();
+            Console.WriteLine($"Возраст - {abc.age}, логин - {abc.login}, Имя - {abc.name}, Фамилия - {abc.surname}, дата - {abc.date}. ");
+            
+
+            Console.ReadKey();
+        }
+    }
+}
+
